341d48a8544eae5758f2d8ed4036dafca03c3238
Golovin_Vladislav <vladick95@gmail.com>
2015-03-28 09:35:20 +0500

    Добавил форму и класс Music, через который можно работать с музыкой

--- IdeaProjects/ZodMelody/MavenZodMelody/src/main/java/Main.java
+++ IdeaProjects/ZodMelody/MavenZodMelody/src/main/java/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.Stage;
 import javafx.event.Event;
+import javafx.stage.StageStyle;
 
 import java.awt.*;
 
@@ -15,15 +16,36 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainForm.fxml"));
+            primaryStage.setTitle("Hello SUPERMAN");
+            primaryStage.setScene(new Scene(root, 506, 443));
+            primaryStage.setResizable(false);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            Controller controller=new Controller();
+            controller.stageControl=primaryStage;
+            primaryStage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 588, 428));
-        primaryStage.show();
+    public void showForm1() {
+        try {
+            Parent e = FXMLLoader.load(this.getClass().getResource("Form1.fxml"));
+            Stage playWindow = new Stage();
+            playWindow.setTitle("Задание 1");
+            playWindow.initStyle(StageStyle.UNDECORATED);
+            playWindow.setScene(new Scene(e, 600.0D, 467.0D));
+            playWindow.setResizable(false);
+            Controller controller = new Controller();
+            controller.stageControl = playWindow;
+            playWindow.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+
 }
